5f1a1fd6cd740aa727ddadf68edd065da9f3df97
Prasiddha Shah <33734132+prasiddha007@users.noreply.github.com>
2024-02-06 14:43:42 -0600

    Update script.js
    Added regex functions

--- script.js
+++ script.js
@@ -7,5 +7,11 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-	const regex = /\+-\s/;
+	const regex = /[+-\s]/g;
+	return str.replace(regex, '');
+}
+
+function isInvalidInput(str) {
+	const regex = /\d+e\d+/i;
+	return str.match(regex);
 }
