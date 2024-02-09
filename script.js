ce7bef4da71f52c528d83b8ac00949aa794d7dd5
Prasiddha Shah <33734132+prasiddha007@users.noreply.github.com>
2024-02-09 10:31:58 -0600

    debugging js file
    Fixed entry counter bug which was starting from 0 instead of 1.
    
    updated addEntry function with insertAdjacentHTML method to preserve input content.
    
    added event listener to call addEntry function.

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function addEntry() {
 		`#${entryDropdown.value} .input-container`
 	);
 	const entryNumber =
-		targetInputContainer.querySelectorAll('input[type="text"]').length;
+		targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
 	const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
   <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
@@ -32,5 +32,7 @@ function addEntry() {
     id="${entryDropdown.value}-${entryNumber}-calories"
     placeholder="Calories"
   />`;
-	targetInputContainer.innerHTML += HTMLString;
+	targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
+
+addEntryButton.addEventListener('click', addEntry);
